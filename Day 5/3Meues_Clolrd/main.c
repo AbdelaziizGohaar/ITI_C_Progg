00369a38e458546bcee5fec58c014976eef29d53
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2024-11-21 05:15:59 +0200

    update menus with employee data entered

--- Day 5/3Meues_Clolrd/main.c
+++ Day 5/3Meues_Clolrd/main.c
@@ -7,9 +7,27 @@
 #define DOWN 66
 #define ENTER 10
 #define EXIT 113
+#define EMP_SIZE 20
 #define clear() printf("\033[H\033[J")
 
+struct BD {
+ unsigned char  Day ;
+ unsigned char Month ;
+ int year ;
+ };
 
+
+ struct Employee {
+ int id ;
+ char name[20] ;
+ int age ;
+ int salary ;
+ struct BD b ;
+ };
+
+void enterNewEmp_void();
+void showMenuAfterExitNew();
+// void addNewUser_void(struct Employee e[]);
 
 int main()
 {
@@ -36,6 +54,7 @@ int main()
 
 
     while(1){
+
 
      char ch =0 ;
     ch = getch();
@@ -150,69 +169,9 @@ int main()
 
             }
             break;
-        }case ENTER: {
-            clear();
-
-            if(currentPos == 15){
-
-            gotoxy(25, 15);
-            textcolor(WHITE);
-            printf("Exit");
-
-             EntryPos = 1;
-
-            } else if(currentPos == 10){
-
-
-            gotoxy(25, 10);
-            textcolor(WHITE);
-            printf("Display");
-
-             EntryPos = 1;
-
-            }else if(currentPos == 5){
-
-            gotoxy(25, 10);
-            textcolor(WHITE);
-            printf("New");
-
-             EntryPos = 1;
-            }
-
-           break;
-        }
-        case EXIT: {
-            clear();
-
-            if(EntryPos == 1){
-
-    gotoxy(25, 5); // Move cursor to column 10, row 5
-    textcolor(BLUE);
-    printf("New");
-
-    textcolor(WHITE);
-
-    gotoxy(25, 10); // Move cursor to column 20, row 8
-    printf("Display");
-
-    gotoxy(25, 15); // Move cursor to column 0, row 10
-    printf("Exit");
-
-       EntryPos = 0 ;
-            } else if(currentPos == 0){
-
-             // printf("we already in main Screen");
-
-            }
-
-            break;
         }
         default:  printf("\n  \n");
     }
-
-
-
-
 
 }
 
@@ -222,18 +181,11 @@ int main()
            case ENTER: {
             clear();
 
-            if(currentPos == 15){
-
-          //  gotoxy(25, 15);
-           // textcolor(WHITE);
-           // printf("Exit");
+            if(currentPos == 15){ /// EXIT
 
               exit(0);
-
              EntryPos = 1;
-
-            } else if(currentPos == 10){
-
+            } else if(currentPos == 10){ /// Display
 
             gotoxy(25, 10);
             textcolor(WHITE);
@@ -241,13 +193,15 @@ int main()
 
              EntryPos = 1;
 
-            }else if(currentPos == 5){
-
-            gotoxy(25, 10);
-            textcolor(WHITE);
-            printf("New");
+            }else if(currentPos == 5){   /// New
 
              EntryPos = 1;
+
+             enterNewEmp_void();      ////////////////////////////////////////////////  method of enter employees /////////////////////////////////////////////////////////////////
+
+             clear();
+
+             showMenuAfterExitNew();  /// show meues again
             }
 
            break;
@@ -291,9 +245,141 @@ int main()
 
 
 //// new Function to add User
+void enterNewEmp_void(){
+ char decistion = 'y';
+ struct Employee emp[EMP_SIZE]={0};
+ int empId = 0;
+ char empName = 0;
+ int empAge = 0 ;
+ int currentIndex = 0;
+
+  while(decistion == 'y')
+  {
+
+  clear();
+
+   printf(" \n Do You Want to enter New User ?  y/n   \n ");
+
+        decistion = getch();
+
+      if(decistion == 'Y' || decistion == 'y'  )
+    {
+      if (currentIndex < EMP_SIZE)
+    {
+     printf("Enter the Employe ID  \n ");
+      scanf("%d",&emp[currentIndex].id);
+
+     printf("Enter the Employe Name  \n ");
+      scanf("%s",&emp[currentIndex].name);
+
+     printf("Enter the Employe age  \n ");
+           scanf("%d",&emp[currentIndex].age);
+/*
+      printf("Enter the Employe Salary  \n ");
+           scanf("%d",&emp[currentIndex].salary);
+
+      printf("Enter the Employe Birth day  \n ");
+           scanf("%d",&emp[currentIndex].b.Day);
+
+      printf("Enter the Employe Birth month  \n ");
+           scanf("%d",&emp[currentIndex].b.Month);
+
+      printf("Enter the Employe Birth year  \n ");
+           scanf("%d",&emp[currentIndex].b.year);
+*/
+        }
+
+        //decistion = 'Y' ;
+     }else if(decistion == 'N' || decistion == 'n'  )
+     {
+        printf("We are in the No Section  \n ");
+        decistion = 'n' ;
+     }else
+     {
+     printf("error please answer with y/n");
+     };
+
+
+  } //end of while loop
+
+
+} // end of Function
 
 
 
+void showMenuAfterExitNew()
+ {
+
+    gotoxy(25, 5); // Move cursor new
+    textcolor(BLUE);
+    printf("New");
+   textcolor(WHITE);
+
+    gotoxy(25, 10); // Move cursor Dis
+    printf("Display");
+
+    gotoxy(25, 15); // Move cursor to exxit
+    printf("Exit");
+
+ }
+
+
+ void displayEmp_Void()
+ {
+ clear();
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// void addNewUser_void(Employee e)
+void addNewUser_void(struct Employee e[])
+ {
+
+     printf("Enter the Employe ID  \n ");
+     scanf("%d",&e.id);
+
+     printf("Enter the Employe Name  \n ");
+      scanf("%c",e.name);
+
+     printf("Enter the Employe age  \n ");
+           scanf("%d",e.age);
+
+      printf("Enter the Employe Salary  \n ");
+           scanf("%d",e.salary);
+
+      printf("Enter the Employe Birth day  \n ");
+           scanf("%d",e.b.Day);
+
+      printf("Enter the Employe Birth month  \n ");
+           scanf("%d",e.b.Month);
+
+      printf("Enter the Employe Birth year  \n ");
+           scanf("%d",e.b.year);
+
+ }
+
+*/
 
 
 
